a53908ffb575a4cef79827e98a139f6dc72208b9
Heathcliff <dennis.nathan@gmail.com>
2016-07-18 15:06:18 -0600

    Added solution code to script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
-test2
+for (x = 0;x < 63;x++) {
+
+  var block = document.createElement('div');
+  block.style.width = '11.1%';
+  block.style.paddingBottom = '11.1%';
+  block.style.float = 'left';
+
+  if (x % 2 === 0) {
+    block.style.backgroundColor = 'red';
+    document.body.appendChild(block);
+  }
+  else {
+    block.style.backgroundColor = 'black';
+    document.body.appendChild(block);
+  }
+}
