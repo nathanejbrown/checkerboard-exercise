3916cbbf747cec954fcf104978994024a1006073
Heathcliff <dennis.nathan@gmail.com>
2016-07-18 15:08:54 -0600

    Added solution code to script.js

--- src/script.js
+++ src/script.js
@@ -1,8 +1,20 @@
-for (x = 96; x > 33; x--) {
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+for (x = 0;x < 63; x++) {
   var block = document.createElement('div');
   block.style.width = '11.1%';
   block.style.paddingBottom = '11.1%';
   block.style.float = 'left';
-  block.style.backgroundColor = '#7452' + x;
+  block.style.backgroundColor = getRandomColor();
+
   document.body.appendChild(block);
 }
+
+window.setTimeout(function() { document.location.reload(true); }, 1000);
